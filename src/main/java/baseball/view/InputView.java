03aee387e2821a9ce463acbe7f885461a49e1308
YongHyeon <pyh4616@naver.com>
2023-11-23 03:21:30 +0900

    feat(InputView): 게임 진행 여부를 입력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,10 +8,16 @@ import java.util.List;
 public class InputView {
 
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_GAME_DECISION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public List<Integer> readPlayerNumber() {
         System.out.print(INPUT_MESSAGE);
         int integer = Convert.stringToInteger(readLine());
         return Convert.integerToList(integer);
+    }
+
+    private int readGameDecision() {
+        System.out.println(INPUT_GAME_DECISION_MESSAGE);
+        return Convert.stringToInteger(readLine());
     }
 }
