3749703f6b66956ead9402b0830a8981170e65a3
YongHyeon <pyh4616@naver.com>
2023-11-23 03:22:43 +0900

    feat(BaseballService): 플레이어와 컴퓨터 숫자를 비교하는 기능 구현

---
+++ src/main/java/baseball/service/BaseballService.java
@@ -0,0 +1,28 @@
+package baseball.service;
+
+import baseball.model.Score;
+import java.util.List;
+
+public class BaseballService {
+
+    private final Score score;
+
+    public BaseballService() {
+        this.score = new Score();
+    }
+
+    public void compareNumber(List<Integer> computer, List<Integer> player) {
+        for (int index = 0; index < 3; index++) {
+            checkIsBallOrStrike(computer, player, index);
+        }
+    }
+
+    private void checkIsBallOrStrike(List<Integer> computer, List<Integer> player, int index) {
+        if (computer.contains(player.get(index))) {
+            if (computer.get(index) != player.get(index)) {
+                score.addBall();
+            }
+            score.addStrike();
+        }
+    }
+}
