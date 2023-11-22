6f04de92a71899d6b53e4e8f1617049b316c2c32
YongHyeon <pyh4616@naver.com>
2023-11-23 02:18:04 +0900

    feat(Convert): 정수를 리스트로 변환하는 기능 구현

--- src/main/java/baseball/util/Convert.java
+++ src/main/java/baseball/util/Convert.java
@@ -1,7 +1,13 @@
 package baseball.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Convert {
 
+    private static final int POSITIVE_CONDITION = 0;
+    private static final int INITIAL_INDEX = 0;
+    private static final int BASE_TEN = 10;
     private static final String CONVERT_ERROR_MESSAGE = "[ERROR] 숫자만 입력하셔야 합니다.";
 
     public static int stringToInteger(String input) {
@@ -10,5 +16,14 @@ public class Convert {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(CONVERT_ERROR_MESSAGE);
         }
+    }
+
+    public static List<Integer> integerToList(int number) {
+        List<Integer> digits = new ArrayList<>();
+        while (number > POSITIVE_CONDITION) {
+            digits.add(INITIAL_INDEX, number % BASE_TEN);
+            number /= BASE_TEN;
+        }
+        return digits;
     }
 }
