dcda28f4aa9d3263fa64882c699eade71bbd8cbf
YongHyeon <pyh4616@naver.com>
2023-11-23 02:56:54 +0900

    refactor(InputView): 플레이어 숫자를 리스트로 반환하도록 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,13 +3,15 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import baseball.util.Convert;
+import java.util.List;
 
 public class InputView {
 
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
-    public int readPlayerNumber() {
+    public List<Integer> readPlayerNumber() {
         System.out.print(INPUT_MESSAGE);
-        return Convert.stringToInteger(readLine());
+        int integer = Convert.stringToInteger(readLine());
+        return Convert.integerToList(integer);
     }
 }
