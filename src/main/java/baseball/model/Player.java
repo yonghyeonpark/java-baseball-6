8f1a0a29eb308f49ebd7a7c0c3de7c6f53b209d5
YongHyeon <pyh4616@naver.com>
2023-11-23 02:24:26 +0900

    refactor(Player): 변수에 final 추가, 매직 넘버를 상수로 선언

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -6,11 +6,13 @@ import java.util.Set;
 
 public class Player {
 
+    private static final int CORRECT_SIZE = 3;
+    private static final int RANGE_STANDARD = 1;
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 세 자릿수를 입력해야 합니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 각 숫자의 범위는 1에서 9까지입니다.";
     private static final String DUPLICATION_ERROR_MESSAGE = "[ERROR] 각 숫자는 중복될 수 없습니다.";
 
-    private List<Integer> player;
+    private final List<Integer> player;
 
     public Player(List<Integer> player) {
         validateSize(player);
@@ -20,7 +22,7 @@ public class Player {
     }
 
     private void validateSize(List<Integer> player) {
-        if (player.size() != 3) {
+        if (player.size() != CORRECT_SIZE) {
             throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
@@ -32,7 +34,7 @@ public class Player {
     }
 
     private void checkRange(int number) {
-        if (number < 1) {
+        if (number < RANGE_STANDARD) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
